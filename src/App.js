0254e15f67e190e766b3ec58326eb05b18ca0621
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2023-02-02 14:24:00 +0530

    React Router DOM

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import SliderImages from './components/SliderImages';
 import CardsSlider from './components/CardsSlider';
 import Premieres from './components/Premieres';
 import Footer from './components/Footer/Footer';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
